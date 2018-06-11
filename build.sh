76f6fcac496cda53cf172e574e8f3ba5e5684e8e
Francesco Ceruti <ceppofrancy@gmail.com>
2018-06-11 12:59:28 +0200

    add override to bintray.json

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ flatpak install flathub org.freedesktop.Platform//1.6 org.freedesktop.Sdk//1.6
 flatpak-builder --verbose --force-clean --ccache build-dir org.python.Python.json
 
 # Put the build result in in tar.gz file
-tar -zcf build.tar.gz -C build-dir/files/ .
+tar -zcf build.tar.gz -C build-dir/files/ bin/ include/ lib/ share/
 
 set +xe
